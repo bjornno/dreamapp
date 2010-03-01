1fc8b002fa0dece59b7c80ab77210c251a690390
Bjrn Nordlund <bjornno@gmail.com>
2010-03-01 20:32:26 +0100

    added ssl support

---
+++ src/main/resources/archetype-resources/src/test/java/ApplicationLauncher.java
@@ -0,0 +1,39 @@
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.security.SslSocketConnector;
+
+
+public class ApplicationLauncher {
+
+    // starts server with plain connection or ssl connection on port 8080, default is
+    // SSL connection.
+    public static void main(String[] args) {
+        Server server = new Server();
+        if (args.length > 1 && "noSSL".equals(args[1])) {
+            Connector defaultConnector = new SocketConnector();
+            defaultConnector.setPort(8080);
+            server.setConnectors(new Connector[] { defaultConnector });
+        } else {
+            SslSocketConnector sslConnector = new SslSocketConnector();
+            sslConnector.setTruststore("src/test/resources/server.truststore.jks");
+            sslConnector.setTrustPassword("123456");
+            sslConnector.setKeystore("src/test/resources/server.keystore.jks");
+            sslConnector.setKeyPassword("123456");
+            sslConnector.setPassword("123456");
+            sslConnector.setPort(8080);
+            sslConnector.setName("SslConnection");
+            sslConnector.setNeedClientAuth(true);
+            server.setConnectors(new Connector[] { sslConnector });
+        }
+        server.addHandler(
+                new org.mortbay.jetty.webapp.WebAppContext("src/main/webapp", "/"));
+        try {
+            server.start();
+        } catch (Exception e) {
+            System.exit(-1);
+        }
+
+    }
+
+}
